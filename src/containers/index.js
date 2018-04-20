00a14178e4030d71383d376d31a4ae5fb8432870
chocobuckle <contact@conordoyle.io>
2018-04-20 23:30:57 +0100

    Add 'Poll' container

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as DashboardContainer } from './DashboardContainer';
 export { default as LeaderboardContainer } from './LeaderboardContainer';
 export { default as AddPollContainer } from './AddPollContainer';
+export { default as PollContainer } from './PollContainer';
